0fe6ab1df853d20c855b5d45533a5ad53a72164b
Dave Esmond <daveesmond@gmail.com>
2017-08-31 11:06:01 -0700

    Commit nothing special

---
+++ Nothing Special/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Nothing Special
+//
+//  Created by David Esmond on 8/31/17.
+//  Copyright © 2017 Esmond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func buttonTapped(_ sender: Any) {
+        lab.text = "Thanks for pushing my button"
+    }
+
+    @IBOutlet weak var myButton: UIButton!
+    @IBOutlet weak var lab: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.red
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
